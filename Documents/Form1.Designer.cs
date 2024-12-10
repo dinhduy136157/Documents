b8416d963f60af59580bdf427c52472ca7d190d1
DinhDuy <dinhduy136157@gmail.com>
2024-12-10 11:20:36 +0700

    Add project files.

---
+++ Documents/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace Documents
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtMaNV = new System.Windows.Forms.TextBox();
+            this.txtHo = new System.Windows.Forms.TextBox();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.cbTrinhDo = new System.Windows.Forms.ComboBox();
+            this.radioNam = new System.Windows.Forms.RadioButton();
+            this.radioNu = new System.Windows.Forms.RadioButton();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.dataTables = new System.Windows.Forms.DataGridView();
+            this.Họ = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ho = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ten = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gioitinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.trinhdo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.diachi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTables)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(162, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã NV";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(162, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Họ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(162, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tên";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(162, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Trình độ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(162, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Giới tính";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(162, 203);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Địa Chỉ";
+            // 
+            // txtMaNV
+            // 
+            this.txtMaNV.Location = new System.Drawing.Point(261, 53);
+            this.txtMaNV.Name = "txtMaNV";
+            this.txtMaNV.Size = new System.Drawing.Size(100, 20);
+            this.txtMaNV.TabIndex = 6;
+            // 
+            // txtHo
+            // 
+            this.txtHo.Location = new System.Drawing.Point(261, 87);
+            this.txtHo.Name = "txtHo";
+            this.txtHo.Size = new System.Drawing.Size(175, 20);
+            this.txtHo.TabIndex = 7;
+            // 
+            // txtTen
+            // 
+            this.txtTen.Location = new System.Drawing.Point(261, 121);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(175, 20);
+            this.txtTen.TabIndex = 8;
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.Location = new System.Drawing.Point(261, 200);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(188, 20);
+            this.txtDiaChi.TabIndex = 9;
+            // 
+            // cbTrinhDo
+            // 
+            this.cbTrinhDo.FormattingEnabled = true;
+            this.cbTrinhDo.Location = new System.Drawing.Point(261, 173);
+            this.cbTrinhDo.Name = "cbTrinhDo";
+            this.cbTrinhDo.Size = new System.Drawing.Size(121, 21);
+            this.cbTrinhDo.TabIndex = 10;
+            // 
+            // radioNam
+            // 
+            this.radioNam.AutoSize = true;
+            this.radioNam.Location = new System.Drawing.Point(261, 145);
+            this.radioNam.Name = "radioNam";
+            this.radioNam.Size = new System.Drawing.Size(47, 17);
+            this.radioNam.TabIndex = 12;
+            this.radioNam.TabStop = true;
+            this.radioNam.Text = "Nam";
+            this.radioNam.UseVisualStyleBackColor = true;
+            // 
+            // radioNu
+            // 
+            this.radioNu.AutoSize = true;
+            this.radioNu.Location = new System.Drawing.Point(322, 145);
+            this.radioNu.Name = "radioNu";
+            this.radioNu.Size = new System.Drawing.Size(39, 17);
+            this.radioNu.TabIndex = 13;
+            this.radioNu.TabStop = true;
+            this.radioNu.Text = "Nữ";
+            this.radioNu.UseVisualStyleBackColor = true;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(582, 48);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 14;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(582, 87);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 15;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(582, 135);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 16;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Location = new System.Drawing.Point(582, 176);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(75, 23);
+            this.btnTimKiem.TabIndex = 17;
+            this.btnTimKiem.Text = "Tìm kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
+            // 
+            // dataTables
+            // 
+            this.dataTables.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataTables.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Họ,
+            this.ho,
+            this.ten,
+            this.gioitinh,
+            this.trinhdo,
+            this.diachi});
+            this.dataTables.Location = new System.Drawing.Point(44, 271);
+            this.dataTables.Name = "dataTables";
+            this.dataTables.Size = new System.Drawing.Size(697, 154);
+            this.dataTables.TabIndex = 18;
+            // 
+            // Họ
+            // 
+            this.Họ.HeaderText = "Mã NV";
+            this.Họ.Name = "Họ";
+            // 
+            // ho
+            // 
+            this.ho.HeaderText = "Họ";
+            this.ho.Name = "ho";
+            // 
+            // ten
+            // 
+            this.ten.HeaderText = "Tên";
+            this.ten.Name = "ten";
+            // 
+            // gioitinh
+            // 
+            this.gioitinh.HeaderText = "Giới Tính";
+            this.gioitinh.Name = "gioitinh";
+            // 
+            // trinhdo
+            // 
+            this.trinhdo.HeaderText = "Trình Độ";
+            this.trinhdo.Name = "trinhdo";
+            // 
+            // diachi
+            // 
+            this.diachi.HeaderText = "Địa chỉ";
+            this.diachi.Name = "diachi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataTables);
+            this.Controls.Add(this.btnTimKiem);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.radioNu);
+            this.Controls.Add(this.radioNam);
+            this.Controls.Add(this.cbTrinhDo);
+            this.Controls.Add(this.txtDiaChi);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.txtHo);
+            this.Controls.Add(this.txtMaNV);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataTables)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtMaNV;
+        private System.Windows.Forms.TextBox txtHo;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.ComboBox cbTrinhDo;
+        private System.Windows.Forms.RadioButton radioNam;
+        private System.Windows.Forms.RadioButton radioNu;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.DataGridView dataTables;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Họ;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ho;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ten;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gioitinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn trinhdo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn diachi;
+    }
+}
+
---
+++ Documents/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Documents
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            HienThi();
+        }
+        XmlDocument doc = new XmlDocument();
+        string tentep = @"C:\Users\Admin\source\repos\Documents\Documents\thongtin.xml";
+        int d;
+        private void HienThi()
+        {
+            dataTables.Rows.Clear();
+            doc.Load(tentep);
+            XmlNodeList DS = doc.SelectNodes("/ds/nhanvien");
+            int sd = 0;
+            dataTables.ColumnCount = 6;
+            dataTables.Rows.Add();
+            foreach (XmlNode nhan_vien in DS)
+            {
+                XmlNode ma_nv = nhan_vien.SelectSingleNode("@manv");
+                dataTables.Rows[sd].Cells[0].Value = ma_nv.InnerText.ToString();
+                XmlNode ho_ten = nhan_vien.SelectSingleNode("hoten");
+                if (ho_ten != null)
+                {
+                    XmlNode ho = ho_ten.SelectSingleNode("ho");
+                    XmlNode ten = ho_ten.SelectSingleNode("ten");
+                    dataTables.Rows[sd].Cells[1].Value = ho?.InnerText; // Họ
+                    dataTables.Rows[sd].Cells[2].Value = ten?.InnerText; // Tên
+                }
+                XmlNode gioi_tinh = nhan_vien.SelectSingleNode("gioitinh"); 
+                dataTables.Rows[sd].Cells[3].Value = gioi_tinh.InnerText.ToString();
+                XmlNode trinh_do = nhan_vien.SelectSingleNode("trinhdo");
+                dataTables.Rows[sd].Cells[4].Value = trinh_do.InnerText.ToString();
+                if (trinh_do != null && !cbTrinhDo.Items.Contains(trinh_do.InnerText))
+                {
+                    cbTrinhDo.Items.Add(trinh_do.InnerText);
+                }
+                XmlNode dia_chi = nhan_vien.SelectSingleNode("diachi");
+                dataTables.Rows[sd].Cells[5].Value = dia_chi.InnerText.ToString();
+                dataTables.Rows.Add();
+                sd++;
+
+            }
+            if (cbTrinhDo.Items.Count > 0)
+            {
+                cbTrinhDo.SelectedIndex = 0;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HienThi();
+        }
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            doc.Load(tentep);
+            XmlElement goc = doc.DocumentElement;
+            XmlNode nhan_vien = doc.CreateElement("nhanvien");
+            XmlAttribute ma_nv = doc.CreateAttribute("manv");
+            ma_nv.InnerText = txtMaNV.Text;
+            nhan_vien.Attributes.Append(ma_nv);
+            //
+            // Tạo phần tử <hoten>
+            XmlNode hoten = doc.CreateElement("hoten");
+
+            // Tạo phần tử <ho> và thêm vào <hoten>
+            XmlNode txt_ho = doc.CreateElement("ho");
+            txt_ho.InnerText = txtHo.Text;
+            hoten.AppendChild(txt_ho);
+
+            // Tạo phần tử <ten> và thêm vào <hoten>
+            XmlNode txt_ten = doc.CreateElement("ten");
+            txt_ten.InnerText = txtTen.Text;
+            hoten.AppendChild(txt_ten);
+
+            // Thêm <hoten> vào <nhanvien>
+            nhan_vien.AppendChild(hoten);
+            // Thêm giới tính
+            XmlNode gioi_tinh = doc.CreateElement("gioitinh");
+            if (radioNam.Checked)
+            {
+                gioi_tinh.InnerText = "Nam";
+            }
+            else if (radioNu.Checked)
+            {
+                gioi_tinh.InnerText = "Nữ";
+            }
+            nhan_vien.AppendChild(gioi_tinh);
+
+            // Thêm trình độ
+            XmlNode trinh_do = doc.CreateElement("trinhdo");
+            trinh_do.InnerText = cbTrinhDo.SelectedItem?.ToString() ?? "";
+            nhan_vien.AppendChild(trinh_do);
+            XmlNode dia_chi = doc.CreateElement("diachi");
+            dia_chi.InnerText = txtDiaChi.Text;
+            nhan_vien.AppendChild(dia_chi);
+            goc.AppendChild(nhan_vien);
+            doc.Save(tentep);
+            HienThi();
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            doc.Load(tentep);
+            XmlElement goc = doc.DocumentElement;
+            XmlNode nhan_vien_cu = goc.SelectSingleNode("/ds/nhanvien[@manv='" + txtMaNV.Text + "']");
+            XmlNode nhan_vien_moi = doc.CreateElement("nhanvien");
+            XmlAttribute ma_nv = doc.CreateAttribute("manv");
+            ma_nv.InnerText = txtMaNV.Text;
+            nhan_vien_moi.Attributes.Append(ma_nv);
+            XmlNode hoten = doc.CreateElement("hoten");
+            XmlNode txt_ho = doc.CreateElement("ho");
+            txt_ho.InnerText = txtHo.Text;
+            // Tạo phần tử <ten> và thêm vào <hoten>
+            XmlNode txt_ten = doc.CreateElement("ten");
+            txt_ten.InnerText = txtTen.Text;
+            hoten.AppendChild(txt_ho);
+            hoten.AppendChild(txt_ten);
+            // Thêm <hoten> vào <nhanvien>
+            nhan_vien_moi.AppendChild(hoten);
+
+            // Xử lý giới tính (RadioButton)
+            XmlNode gioi_tinh = doc.CreateElement("gioitinh");
+            if (radioNam.Checked)
+            {
+                gioi_tinh.InnerText = "Nam";
+            }
+            else if (radioNu.Checked)
+            {
+                gioi_tinh.InnerText = "Nữ";
+            }
+            nhan_vien_moi.AppendChild(gioi_tinh);
+
+            // Xử lý trình độ (ComboBox)
+            XmlNode trinh_do = doc.CreateElement("trinhdo");
+            trinh_do.InnerText = cbTrinhDo.SelectedItem?.ToString() ?? "";
+            nhan_vien_moi.AppendChild(trinh_do);
+
+            XmlNode dia_chi = doc.CreateElement("diachi");
+            dia_chi.InnerText = txtDiaChi.Text;
+            nhan_vien_moi.AppendChild(dia_chi);
+            goc.ReplaceChild(nhan_vien_moi, nhan_vien_cu);
+            doc.Save(tentep);
+            HienThi();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            doc.Load(tentep);
+            XmlElement goc = doc.DocumentElement;
+            XmlNode nhan_vien_xoa = goc.SelectSingleNode("/ds/nhanvien[@manv='" + txtMaNV.Text + "']");
+            goc.RemoveChild(nhan_vien_xoa);
+            doc.Save(tentep);
+            HienThi();
+        }
+
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            XmlNodeList DS = doc.SelectNodes("/ds/nhanvien");
+            string manvtimkiem = txtMaNV.Text;
+            bool found = false;
+
+            foreach (XmlNode nhan_vien in DS)
+            {
+                XmlNode manvne = nhan_vien.SelectSingleNode("@manv");
+                if (manvne != null && manvne.InnerText == manvtimkiem)
+                {
+                    XmlNode nameNode = nhan_vien.SelectSingleNode("hoten");
+                    XmlNode diachi = nhan_vien.SelectSingleNode("diachi");
+                    string thongTinTimKiem = $"Mã NV: {manvne.InnerText}\nHọ Tên: {nameNode?.InnerText}\nĐịa chỉ: {diachi?.InnerText}";
+                    MessageBox.Show(thongTinTimKiem, "Thông tin nhân viên", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    found = true;
+                    break;
+                }
+            }
+        }
+    }
+}
